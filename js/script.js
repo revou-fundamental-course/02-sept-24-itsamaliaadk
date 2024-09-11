138aec71e77055ef54e5904bfe478498e824546f
Amalia Dewii <amaliaadk26@gmail.com>
2024-09-11 20:08:07 +0700

    update jss & css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // This is javascript
+
+function validateLuas() {
+  let panjangAlas = document.getElementById("panjang-alas").value;
+  let tinggi = document.getElementById("tinggi").value;
+
+  if (
+    panjangAlas == "" || tinggi == "" || panjangAlas.includes("e") || tinggi.includes("e")) {
+    alert("Form Masih Kosong");
+  }
+  
+  console.log(panjangAlas);
+  console.log(tinggi);
+}
